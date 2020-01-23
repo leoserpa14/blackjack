52e96040ad5aa82ba9e747b7e2295f97f79c6156
leoserpa14 <lscordeiro14@gmail.com>
2020-01-22 21:08:59 -0300

    Blackjack game finished

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from random import shuffle
 
 valores = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10,
@@ -30,8 +29,6 @@ class Jogador:
 		self.hand = []
 
 
-
-
 class Mesa:
 	def __init__(self, dealer, jogador, deck):
 		self.dealer = dealer
@@ -48,10 +45,6 @@ baralho = Baralho().create()
 deck = baralho * 6
 shuffle(deck)
 
-# for i in range(52*6):
-# 	print(deck[i].info)
-
-
 jogador = Jogador()
 dealer = Jogador()
 
@@ -62,19 +55,26 @@ mesa.hit(mesa.jogador)
 mesa.hit(mesa.dealer)
 
 
-print("Mão do Dealer: " + mesa.dealer.hand[0].info + " e " + mesa.dealer.hand[1].info)
+print("Mão do Dealer: " + mesa.dealer.hand[0].info)
 print("Sua mão: " + mesa.jogador.hand[0].info + " e " + mesa.jogador.hand[1].info)
 
 
 mao_jogador = [mesa.jogador.hand[0].valor, mesa.jogador.hand[1].valor]
 pontos = sum(mao_jogador)
+
+if pontos == 21:
+	print("Blackjack! Você ganhou!")
+	exit()
+
 while True:
 	n = input("Quer mais uma carta? (S/N): ")
+
 	if n == "S" or n == "s":
 		# Pega uma carta
 		mesa.hit(mesa.jogador)
 		# Print as cartas que vc tem na mão
 		a = len(mesa.jogador.hand)
+		print("Sua mão: ")
 		for i in range(a):
 			print(mesa.jogador.hand[i].info + " (" + str(mesa.jogador.hand[i].valor) + ")")
 		mao_jogador.append(mesa.jogador.hand[a-1].valor)
@@ -83,13 +83,14 @@ while True:
 		pontos = sum(mao_jogador)
 		print("Total: " + str(pontos))
 
-		# if pontos > 21:
-		# 	print("Você perdeu!")
-		# 	break
-		#
-		# if pontos == 21:
-		# 	print("Blackjack!")
-		# 	break
+		# Busted!
+		if pontos > 21:
+			print("Você perdeu!")
+			break
+		# Com 21 vc não vai querer mais cartas
+		if pontos == 21:
+			print("21!")
+			break
 
 	if n == "N" or n == "n":
 		break
@@ -99,28 +100,25 @@ print("\nMão do Dealer: " + mesa.dealer.hand[0].info + " e " + mesa.dealer.hand
 mao_dealer = [mesa.dealer.hand[0].valor, mesa.dealer.hand[1].valor]
 
 while True:
-
 	pontos_dealer = sum(mao_dealer)
-	
 	if pontos_dealer < 16:
 		mesa.hit(mesa.dealer)
 		a = len(mesa.dealer.hand)
-		# for i in range(a):
-		# 	print(mesa.dealer.hand[i].info + " (" + str(mesa.dealer.hand[i].valor) + ")")
 		print("Dealer recebeu " + mesa.dealer.hand[a-1].info)
 		mao_dealer.append(mesa.dealer.hand[a-1].valor)
-	else:
+	elif 16 <= pontos_dealer < 21:
 		break
+	if pontos_dealer > 21:
+		print("Dealer busted! Você ganhou!")
+		exit()
 
-print("\n\nResultado final\n ")
+print("\nResultado final\n ")
 len_d = len(mesa.dealer.hand)
 len_j = len(mesa.jogador.hand)
 
 print("Mão do Dealer: " + str(pontos_dealer))
 for i in range(len_d):
 	print(mesa.dealer.hand[i].info)
-
-
 
 print("\nSua mão: " + str(pontos))
 for i in range(len_j):
@@ -131,7 +129,7 @@ if pontos > pontos_dealer:
 	print("\nJogador campeão!")
 
 if pontos < pontos_dealer:
-	print("\nDealer campeão!")
+	print("\nVocê perdeu!")
 
 if pontos == pontos_dealer:
 	print("\nEmpate!")
